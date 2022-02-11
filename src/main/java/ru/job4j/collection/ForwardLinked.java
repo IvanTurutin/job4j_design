4e63ef137e7063707fc929898ff31f0e5e596877
Ivan <ivan.turutin@gmail.com>
2022-02-11 10:30:55 +0300

    6. Перевернуть связанный список [#161 #263096]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -40,21 +40,21 @@ public class ForwardLinked<T> implements Iterable<T> {
     }
 
     public boolean revert() {
-        if (head == null || head.next == null) {
-            return false;
+        boolean rsl = false;
+        if (head != null && head.next != null) {
+            Node<T> previous = null;
+            Node<T> current = head;
+            Node<T> next;
+            while (current != null) {
+                next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+            }
+            head = previous;
+            rsl = true;
         }
-        Node<T> previous = null;
-        Node<T> current = head;
-
-        while (current != null) {
-            Node<T> next = current.next;
-            current.next = previous;
-            previous = current;
-            current = next;
-        }
-        head = previous;
-
-        return true;
+        return rsl;
     }
 
     @Override
