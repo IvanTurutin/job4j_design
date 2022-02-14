0fe8dc7ff39325649e910cab524f6e30453bd7b5
Ivan <ivan.turutin@gmail.com>
2022-02-14 19:33:50 +0300

    1. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserTest {
+    public static void main(String[] args) {
+        Calendar birthday = new GregorianCalendar(1988, 1, 15);
+        String name = "Ivan";
+        int children = 2;
+
+        System.out.println(birthday.getTime());
+        User user1 = new User(name, children, birthday);
+        User user12 = new User(name, children, birthday);
+        User user2 = new User(name, children, birthday);
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        map.keySet().stream().forEach(System.out::println);
+
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
+
+        System.out.println(hash(user1));
+        System.out.println(hash(user2));
+
+    }
+
+    static final int hash(User key) {
+        int h;
+
+        return (key == null) ? 0 : ((h = key.hashCode()) ^ (h >>> 16)) & 15;
+
+    }
+
+}
