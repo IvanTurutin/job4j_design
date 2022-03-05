6216aa41b8cb4472140b9ef79af83132cb902d24
Ivan <ivan.turutin@gmail.com>
2022-03-05 18:56:44 +0300

    4.2. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -9,8 +11,19 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".java")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Arguments entered incompletely. Usage java -jar Search.jar ROOT_FOLDER FILE_EXTENSION.");
+        }
+
+        Path start;
+        try {
+            start = Paths.get(args[0]);
+        } catch (InvalidPathException e) {
+            throw new IllegalArgumentException("First argument must be Path format. For example c:/projects");
+        }
+
+        String extension = args[1];
+        search(start, p -> p.toFile().getName().endsWith(extension)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
